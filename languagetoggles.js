bda8e573ef520d216208b3e0b96911f00a95b584
xmslvs <xmslvs@gmail.com>
2024-07-29 22:13:28 +0900

    created languagetoggles.js, added system for showing dual languages

---
+++ languagetoggles.js
@@ -0,0 +1,23 @@
+function languageToggle1() { 
+    var Japanese = document.getElementById("japanese1");
+    var English = document.getElementById("english1");
+    if (Japanese.style.display === "none") {
+        Japanese.style.display = "block";
+        English.style.display = "none";
+    } else {
+        Japanese.style.display = "none";
+        English.style.display = "block";
+    }
+    };
+
+    function languageToggle2() { 
+        var Japanese = document.getElementById("japanese2");
+        var English = document.getElementById("english2");
+        if (Japanese.style.display === "none") {
+            Japanese.style.display = "block";
+            English.style.display = "none";
+        } else {
+            Japanese.style.display = "none";
+            English.style.display = "block";
+        }
+        };
